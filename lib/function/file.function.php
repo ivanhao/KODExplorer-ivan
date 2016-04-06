dc8f92c20c6c1e0fac2197d6a9542c77604fe0b6
ivanhao <ivanhao1984@qq.com>
2016-04-06 09:25:29 +0800

    3.21-support big file size

--- lib/function/file.function.php
+++ lib/function/file.function.php
@@ -49,10 +49,47 @@ function iconv_system($str){
 	return $result;
 }
 
-function get_filesize($path){
-	@$ret = abs(sprintf("%u",filesize($path))); 
-	return (int)$ret;
+function fsize($file) {
+    // filesize will only return the lower 32 bits of
+    // the file's size! Make it unsigned.
+    $fmod = filesize($file);
+      if ($fmod < 0) $fmod += 2.0 * (PHP_INT_MAX + 1);
+
+        // find the upper 32 bits
+        $i = 0;
+
+          $myfile = fopen($file, "r");
+
+            // feof has undefined behaviour for big files.
+            // after we hit the eof with fseek,
+            // fread may not be able to detect the eof,
+            // but it also can't read bytes, so use it as an
+            // indicator.
+            while (strlen(fread($myfile, 1)) === 1) {
+                  fseek($myfile, PHP_INT_MAX, SEEK_CUR);
+                      $i++;
+                        }
+
+              fclose($myfile);
+
+                // $i is a multiplier for PHP_INT_MAX byte blocks.
+                // return to the last multiple of 4, as filesize has modulo of 4 GB (lower 32 bits)
+                if ($i % 2 == 1) $i--;
+
+                  // add the lower 32 bit to our PHP_INT_MAX multiplier
+                  return ((float)($i) * (PHP_INT_MAX + 1)) + $fmod;
 }
+
+function get_filesize($path){
+      @$ret = fsize($path);
+          return $ret;
+}
+//***This is old mathod.***
+//function get_filesize($path){
+//	@$ret = abs(sprintf("%u",filesize($path))); 
+//	return (int)$ret;
+//}
+
 /**
  * 获取文件详细信息
  * 文件名从程序编码转换成系统编码,传入utf8，系统函数需要为gbk
